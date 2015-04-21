1cb82395bedf30f49ccb0d5b73dc97dbeeb3c0a5
Don Hey <donhey@gmail.com>
2015-04-21 18:53:36 -0400

    Some troubleshooting code was added to server.js, which is now a mess.

--- server.js
+++ server.js
@@ -30,7 +30,34 @@ app.use(function(req, res, next) {
 app.use(morgan('dev'));
 
 // Connect to the database:
-mongoose.connect('mongodb://localhost:27017/openBliki'); 
+// mongoose.connect = ('mongodb://dhey:malvingi@ds055689.mongolab.com:55689/tryagain', function (err, res)
+// mongoose.connect = ('mongodb://dhey:malvingi@ds061158.mongolab.com:61158/openbliki', function (err, res)
+mongoose.connect('mongodb://localhost:27017/openBliki', function (err, res)
+{
+    if (err)
+    {
+        console.log('Error connecting to Mongodb: ' + err);
+    }
+
+    else
+    {
+        console.log('Connected to Mongodb');
+    }
+});
+
+mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
+
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function (callback) {
+//   // yay!
+// });
+
+// var mongodb = require('mongodb');
+// var uri = 'mongodb://dhey:malvingi@ds061158.mongolab.com:61158/openbliki';
+// mongodb.MongoClient.connect(uri, function (err, db) {
+//     if (err) {console.log(err); }
+// });
 
 // Set static files location used for requests that our front end will make:
 app.use(express.static(__dirname + '/public'));
