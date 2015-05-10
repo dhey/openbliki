34991b4685494e50dcf73612b06f968d4bb30297
Don Hey <donhey@gmail.com>
2015-05-10 09:32:24 -0400

    The article route was factored out into ArticleController.

---
+++ app/controllers/ArticleController.js
@@ -0,0 +1,21 @@
+var Article = require('../models/article');
+
+module.exports.controller = function(apiRouter) {
+apiRouter.get('/article', function(req, res)
+{
+    console.log("Retrieving the latest article...");
+
+    Article.find({}).sort({_id: 'desc'}).limit(1).exec(function(err, article)
+    {
+        if (err)
+        {
+            return res.send(err);
+        }
+
+        console.log(article);
+
+            // Return the articles:
+            res.json(article);
+        });
+});
+};
