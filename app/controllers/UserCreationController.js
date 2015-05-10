f99c3e5f4faf0bb6f3a0dff93b6c8bf6f457a721
Don Hey <donhey@gmail.com>
2015-05-10 10:21:46 -0400

    UserCreationController was factored out.

---
+++ app/controllers/UserCreationController.js
@@ -0,0 +1,40 @@
+var User = require('../models/user');
+
+module.exports.controller = function(apiRouter) 
+{
+    // Create a user (accessed at POST http://localhost:8080/users)
+    apiRouter.post('/users', function(req, res) 
+    {
+        // Create a new instance of the User model:
+        var user = new User();      
+        user.name = req.body.name;
+        user.username = req.body.username;
+        user.password = req.body.password;
+
+        console.log("Creating a new user:");
+        console.log("Name: " + user.name);
+        console.log("Username: " + user.username);
+        console.log("Password: " + user.password);
+
+        user.save(function(err) 
+        {
+            if (err) 
+            {
+                // Check for the duplicate entry error code:
+                if (err.code === 11000)
+                {
+                    return res.json(
+                    {
+                        success: false, 
+                        message: 'A user with that username already exists.'
+                    });
+                } 
+
+                return res.send(err);
+            }
+
+            // Return a message
+            res.json({ message: 'User created!', id : user.id });
+        });
+    });
+};
--- app/routes/api.js
+++ app/routes/api.js
@@ -1,12 +1,11 @@
 var User = require('../models/user');
 var Article = require('../models/article');
-var jwt = require('jsonwebtoken');
-var superSecret = 'bourbon';
 var express = require('express');
 var apiRouter = module.exports = express();
 var fs = require('fs');
 var path = require('path');
-// var ArticleController = require('../controllers/ArticleController');
+var jwt = require('jsonwebtoken');
+var superSecret = 'bourbon';
 
 
 // dynamically include routes (Controller)
@@ -53,49 +52,11 @@ apiRouter.use(function(req, res, next)
 			success: false,
 			message: 'No token was provided.'
 		});
-	}a
+	}
 });
 
 // On routes that end in /users
 apiRouter.route('/users')
-
-		// Create a user (accessed at POST http://localhost:8080/users)
-		.post(function(req, res) 
-		{
-			
-			// Create a new instance of the User model:
-			var user = new User();		
-			user.name = req.body.name;
-			user.username = req.body.username;
-			user.password = req.body.password;
-
-			console.log("Creating a new user:");
-			console.log("Name: " + user.name);
-			console.log("Username: " + user.username);
-			console.log("Password: " + user.password);
-			
-			user.save(function(err) 
-			{
-				if (err) 
-				{
-					// Check for the duplicate entry error code:
-					if (err.code === 11000)
-					{
-						return res.json(
-						{
-							success: false, 
-							message: 'A user with that username already exists.'
-						});
-					} 
-
-					return res.send(err);
-				}
-
-				// Return a message
-				res.json({ message: 'User created!', id : user.id });
-			});
-
-		})
 
 		// Get all the users (accessed at GET http://localhost:8080/api/users)
 		.get(function(req, res) 
